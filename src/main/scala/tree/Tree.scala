3f2a3692870af04562c686ab953f7a8bb42937e7
Hitoshi ASAI <hitoshi@hfedora.localdomain>
2010-11-23 13:55:46 +0900

    objective tree

--- src/main/scala/tree/Tree.scala
+++ src/main/scala/tree/Tree.scala
@@ -1,40 +1,76 @@
 package tree
 
-object Tree extends Application {
-  private var dirNum = 0;
-  private var fileNum = 0;
+import java.io.File
 
-  private def tree(dir: java.io.File, branch: String): List[String] = {
-    var curStr:List[String] = Nil
-    val files = dir.listFiles.toList.
-                filter(!_.getName.startsWith(".")).reverse
-
-    for (i <- 0 until files.length) {
-      val name = files(i).getName
-      val curBranch1 = if (i == files.length - 1) "    "
-                       else "|   "
-      val curBranch2 = if (i == files.length - 1) "`-- "
-                       else "|-- "
-
-      if (files(i).isDirectory) {
-        dirNum += 1
-        curStr = tree(files(i), branch + curBranch1) :::
-                  ((branch + curBranch2 + name) :: curStr)
-      } else {
-        fileNum += 1
-        curStr = (branch + curBranch2 + name) :: curStr
-      }
+object Tree {
+  
+  def main(args: Array[String]) {
+    val dirName = args match {
+      case Array(hd) => hd
+      case Array(hd, _) => hd
+      case _ => "."
     }
-    curStr
-  }
-
-  override def main(args: Array[String]): Unit = {
-    try {
-      val treeList = tree(new java.io.File(args(0)), "") ::: List(".")
-      println(treeList.reverse.mkString("\n") +
-              "\n\n%d directories, %d files".format(dirNum, fileNum))
-    } catch {
-      case e: NullPointerException => println("[error opening dir]")
+    new File(dirName) match {
+      case d if d.isDirectory => new RootDir(d).print
+      case d => {
+        println("[Error] " + d + " is not directory!")
+        exit(1)
+      }
     }
   }
 }
+
+abstract class Node(file : File, indent: String = "") {
+  
+  val branch : String
+  val childNode : String
+  
+  def print {
+    println(indent + branch + file.getName)
+    if (file.isDirectory)
+      ls.foreach(_.print)
+  }
+
+  private def ls : Array[Node] = {
+    try {
+      val l = file.listFiles.zipWithIndex
+      l.collect { 
+        case (f, i) if f.isDirectory & i == l.length - 1 => new LastDir(f, indent + childNode)
+        case (f, i) if f.isFile & i == l.length - 1 => new LastFile(f, indent + childNode)
+        case (f, i) if f.isDirectory => new NormalDir(f, indent + childNode)
+        case (f, i) if f.isFile => new NormalFile(f, indent + childNode)
+      }
+    } catch {
+      case e: NullPointerException => println("Permission Denied. [%s]".format(file))
+      Array()
+    }
+  }
+}
+
+class RootDir(file : File, indent : String = "") extends Node(file, indent) {
+  val branch = ""
+  val childNode = ""
+}
+
+class NormalDir(file : File, indent : String) extends Node(file, indent) {
+  val branch = "|-- "
+  val childNode = "|   "
+}
+
+class NormalFile(file : File, indent : String) extends Node(file, indent) {
+  val branch = "|-- "
+  val childNode = ""
+}
+
+class LastDir(file : File, indent : String) extends Node(file, indent) {
+  val branch = "`-- "
+  val childNode = "    "
+}
+
+class LastFile(file : File, indent : String) extends Node(file, indent) {
+  val branch = "`-- "
+  val childNode = ""
+}
+
+
+
